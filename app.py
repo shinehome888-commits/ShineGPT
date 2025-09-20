800d3665f2fb69f92a44edf36af34e1a203eb8b7
King Solomon I <shinehome888@gmail.com>
2025-09-19 18:04:12 -0700

    app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def load_online_model():
             MODEL_NAME,
             torch_dtype="auto",
             device_map="auto",
-            trust_remote_code=true,
+            trust_remote_code=True,
             low_cpu_mem_usage=True,
         )
         return tokenizer, model
